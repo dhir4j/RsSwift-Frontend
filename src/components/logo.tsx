61d5faea4dffc20978185f24dac8d3a731623ba9
dhir4j <djkapse007@gmail.com>
2025-07-04 17:09:06 +0000

    change logo in menu bar for both light and dark mode to  public/images/r

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,23 +2,12 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <>
-      <Image
-        src="/images/rsswift_light_mode.png"
-        alt="RS SWIFT COURIERS Logo"
-        width={140}
-        height={22}
-        className="dark:hidden"
-        priority
-      />
-      <Image
-        src="/images/rsswift_dark_mode.png"
-        alt="RS SWIFT COURIERS Logo"
-        width={140}
-        height={22}
-        className="hidden dark:block"
-        priority
-      />
-    </>
+    <Image
+      src="/images/rsswift_logo.png"
+      alt="RS SWIFT COURIERS Logo"
+      width={140}
+      height={22}
+      priority
+    />
   );
 }
