7ca15abba907ccc2aada2ba1d68ecfa31203b1df
dhir4j <djkapse007@gmail.com>
2025-07-04 18:26:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/components/shipment/book-shipment-form.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+
+export function BookShipmentForm() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Book a Shipment</CardTitle>
+        <CardDescription>Shipment booking form will be here.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>This is a placeholder for the shipment booking form.</p>
+      </CardContent>
+    </Card>
+  );
+}
