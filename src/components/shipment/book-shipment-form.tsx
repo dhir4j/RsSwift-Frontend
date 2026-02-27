d4a9301e97fc3d0853c303d829cb1a87f756e887
dhir4j <kapsedhiraj15@gmail.com>
2026-02-27 11:15:14 +0530

    fixed doesnt allow old users

--- src/components/shipment/book-shipment-form.tsx
+++ src/components/shipment/book-shipment-form.tsx
@@ -395,7 +395,7 @@ export function BookShipmentForm() {
                   <div className="grid md:grid-cols-2 gap-4">
                     <FormField control={form.control} name="pickupDate" render={({ field }) => (<FormItem className="flex flex-col"><FormLabel>Pickup Date</FormLabel><Popover><PopoverTrigger asChild><FormControl>
                       <Button variant={"outline"} className={cn("w-[240px] pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>{field.value ? format(field.value, "PPP") : <span>Pick a date</span>}<CalendarIcon className="ml-auto h-4 w-4 opacity-50" /></Button>
-                      </FormControl></PopoverTrigger><PopoverContent className="w-auto p-0" align="start"><Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus /></PopoverContent></Popover><FormMessage /></FormItem>
+                      </FormControl></PopoverTrigger><PopoverContent className="w-auto p-0" align="start"><Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={user?.email !== 'admin@shedloadoverseas.com' ? { before: new Date() } : undefined} initialFocus /></PopoverContent></Popover><FormMessage /></FormItem>
                     )} />
                     {shipmentTypeOption === 'Domestic' && <FormField control={form.control} name="serviceType" render={({ field }) => (
                         <FormItem>
